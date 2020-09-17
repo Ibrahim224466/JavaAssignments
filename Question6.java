bf0cf8ed66cd1f58ac8a68351f4ef9baf05c5d94
Ibrahim224466 <71451583+Ibrahim224466@users.noreply.github.com>
2020-09-17 18:37:31 +0200

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,32 @@
+package assignments.day_2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		
+		System.out.println("\t   ^    ");
+		System.out.println("\t  / \\ ");
+		System.out.println("\t /   \\ ");
+		System.out.println("\t/     \\ ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t------- ");
+		
+		
+
+		
+		
+		
+		
+	}
+
+}
